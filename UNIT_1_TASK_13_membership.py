2c124b09ea4cea6b67db3cfc107cde91ed59203e
prasunambhasakalabattula <prasunambhasakalabattula@gmail.com>
2026-09-18 15:59:46 +0530

    Add files via upload

---
+++ UNIT_1_TASK_13_membership.py
@@ -0,0 +1,10 @@
+b='a' in 'apple'
+print("result of b:" , b)
+b='a'not in 'apple'
+print("result of b:" , b)
+b='z' in 'apple'
+print("result of b:" , b)
+b='z' not in 'apple'
+print("result of b:" , b)
+b='m' not in 'apple'
+print("result of b:" , b)
