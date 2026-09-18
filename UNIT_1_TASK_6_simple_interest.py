ec331d7fd927a81e701ba59b175605b5ef94a160
prasunambhasakalabattula <prasunambhasakalabattula@gmail.com>
2026-09-18 15:54:17 +0530

    Add files via upload

---
+++ UNIT_1_TASK_6_simple_interest.py
@@ -0,0 +1,6 @@
+#Aim:write a program to find the simle interest.
+principal=float(input("Enter the principal amount: "))
+rate=float(input("Enter the rate of interest: "))  
+time=float(input("Enter the time period: "))
+simple_interest=(principal*rate*time)/100
+print("The simple interest is: ",simple_interest)
