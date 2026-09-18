0e3ffb307c20bfc183c4e0a689f017017d5751e4
prasunambhasakalabattula <prasunambhasakalabattula@gmail.com>
2026-09-18 15:58:47 +0530

    Add files via upload

---
+++ UNIT_1_TASK_12_identity_operators.py
@@ -0,0 +1,9 @@
+#identity operators exercise
+a=[1,2,3]
+b=a
+result=b is a #is identity operators
+print("result of",b,"is",a,"is :",result)
+c=[1,2,3]
+d=[1,2,3]
+result=d is c # is identity operators
+print("result of",d,"is :",result)
