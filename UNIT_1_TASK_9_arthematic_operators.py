6710ed527b44b896fd463074399bd9780f1c5af8
prasunambhasakalabattula <prasunambhasakalabattula@gmail.com>
2026-09-18 15:55:22 +0530

    Add files via upload

---
+++ UNIT_1_TASK_9_arthematic_operators.py
@@ -0,0 +1,9 @@
+a = 7
+b = 2
+print("addition:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
+print("modulus:",a%b)
+print("floor division:",a//b)
+print("exponent:",a**b)
