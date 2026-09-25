7b016b2a22f48187b670b813b2685561a0a847ed
prasunambhasakalabattula <prasunambhasakalabattula@gmail.com>
2026-09-25 19:25:31 +0530

    Add files via upload

---
+++ UNIT-1_TASK-14_OperatorPrescedence.py
@@ -0,0 +1,11 @@
+# Operator Precedence and Associativity
+#example of operator precedence and associativity
+x = 10
+y = 5
+# Using parentheses to change the order of operations
+# Without parentheses: 10 + 10 = 20
+result1 = x + y * 2  
+# with paranthesis: 15 * 2 =30 
+result2 = (x + y) * 2
+print(f"Without parentheses: {result1}")
+print(f"With parentheses: {result2}")
