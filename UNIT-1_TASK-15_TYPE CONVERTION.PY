e7b0c9a559a57372c39d4317a0f596ba178617d6
prasunambhasakalabattula <prasunambhasakalabattula@gmail.com>
2026-09-25 19:29:17 +0530

    Add files via upload

---
+++ UNIT-1_TASK-15_TYPE CONVERTION.PY
@@ -0,0 +1,5 @@
+# Type Conversion 
+type1 = int(input("Enter a number: "))
+type2 = float(input("Enter a decimal number: "))
+type3 = str(input("Enter a string: "))
+print(f"Integer: {type1}, Float: {type2}, String: {type3}")
